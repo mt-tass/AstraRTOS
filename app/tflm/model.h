8238f230a1b0f0f72a7ae34f9b3213a1b0f78439
Mustafa Taher <mustafataherramp@gmail.com>
2026-07-27 22:41:43 +0530

    update model files

--- app/tflm/model.h
+++ app/tflm/model.h
@@ -1,7 +1,15 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern const unsigned char hello_world_float_tflite[];
 extern const unsigned int hello_world_float_tflite_len;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
